3c7f1148a2a1278f51161155afa35384a0b47861
LiliaBerberyan <98405093+LiliaBerberyan@users.noreply.github.com>
2022-04-26 18:23:40 +0400

    Add files via upload
    first commit

---
+++ MyBooks/Controllers/AuthorsController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MyBooks.Data.Services;
+using MyBooks.Data.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyBooks.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthorsController : ControllerBase
+    {
+        //we need to inject Authors Service
+        private AuthorsService _authorsService;
+        public AuthorsController(AuthorsService authorsService)
+        {
+            _authorsService = authorsService;
+        }
+
+        [HttpPost("add-author")]
+        public IActionResult AddBook([FromBody] AuthorVM author)
+        {
+            _authorsService.AddAuthor(author);
+            return Ok();
+        }
+
+        [HttpGet("get-author-with-books-by-id/{id}")]
+        public IActionResult GetAuthorWithBooks(int id)
+        {
+            var response = _authorsService.GetAuthorWithBooks(id);
+            return Ok(response);
+        }
+    }
+}
